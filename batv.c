729744976c0e653b090127bd4f1b43fa23b2ed16
Max <max@pengi.se>
2010-08-16 15:53:30 +0000

    Added copyright string
    git-svn-id: file:///Volumes/Archive/dump/moriarty/projects/svn/aprstrack/trunk@3 99391fad-4a4d-4a94-8e5a-8e059c8e869a

--- batv.c
+++ batv.c
@@ -1,6 +1,8 @@
 /*
     This file is part of APRStrack.
 
+    Copyright (C) 2010 Max Sikström / SA6BBC
+
     APRStrack is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
